c5881298c417132c4d8d43ee2b0706149abda178
Tatjana <bezcennajabtv@gmail.com>
2022-04-19 18:36:29 +0300

    create my-react-site

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./app.scss";
+import Main from "./pages/main/Main.page";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+};
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello, Tania!</h1>
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ src/pages/main/Main.page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./main.page.scss";
+
+const Main = () => {
+  return (
+    <>
+      <h1>Hello, my name is Tatjana, i`m FrontEnd developer</h1>
+    </>
+  );
+};
+
+export default Main;
