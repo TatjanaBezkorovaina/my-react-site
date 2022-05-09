0d6255f6d2109ceb7c48c82825b1b6f26ba6bf2b
Tatjana <bezcennajabtv@gmail.com>
2022-05-09 14:19:03 +0300

    change size button game and add tag in main page

--- src/pages/main/Main.page.jsx
+++ src/pages/main/Main.page.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import "./main.page.scss";
+import { Icon } from "@iconify/react";
 
 const Main = () => {
   const { t } = useTranslation();
   return (
     <>
-      <div>
-        <div className="title__wrapper">
-          <h1 className="title">{t("name")}</h1>
-          <br />
-          <h2 className="subtitle">{t("specialization")}</h2>
+      <div className="title__wrapper">
+        <h1 className="title">{t("name")}</h1>
+        <br />
+        <h2 className="subtitle">{t("specialization")}</h2>
+      </div>
+      <div className="right-part">
+        <div className="tag">
+          <Icon className="tag__icon" icon="logos:google-tag-manager" />
         </div>
       </div>
     </>
